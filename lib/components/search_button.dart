d83f51e4a9095d0527c94d62d8367e76529a5185
viancie <viancie.ocena@gmail.com>
2023-12-27 00:58:45 +0800

    yawa

---
+++ lib/components/search_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SearchButton extends StatelessWidget {
+  const SearchButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, '/search_page'),
+      child: Container(
+        //padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(children: [
+          Icon(
+            Icons.search,
+            color: Colors.grey[800],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            "What do you want to learn",
+            style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 16,
+                fontWeight: FontWeight.w500),
+          )
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 244, 253),
+    );
+  }
+}
