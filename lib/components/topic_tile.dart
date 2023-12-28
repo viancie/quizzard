7a006602774ca2ab8d5968736474d865a8a75cdd
viancie <viancie.ocena@gmail.com>
2023-12-28 21:26:52 +0800

    data repo, files connected except quiz, home page done tong masibog2 chuchu, bookmark page done, topic view page done, subtopic view page ?done (references not included)

--- lib/components/topic_tile.dart
+++ lib/components/topic_tile.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:quizzard/components/heart_button.dart';
+import 'package:quizzard/model/topic.dart';
+import 'package:quizzard/pages/topicView_page.dart';
 
 class TopicTile extends StatelessWidget {
-  const TopicTile({super.key});
+  final Topic topic;
+  final Color color;
+  final String imageUrl;
+  const TopicTile(
+      {super.key,
+      required this.topic,
+      required this.color,
+      required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, 'topicView_page'),
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => TopicViewPage(topic: topic))),
       child: Container(
         width: 250,
         height: 265,
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
-          color: Colors.orange,
+          color: color,
           borderRadius: BorderRadius.circular(12),
         ),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -21,25 +32,25 @@ class TopicTile extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset(
-                'lib/assets/topic_image.png',
+              SvgPicture.asset(
+                imageUrl,
                 height: 120,
               ),
               HeartButton(
-                isLiked: false,
+                isLiked: topic.getIsLiked,
                 onTap: () {},
               )
             ],
           ),
-          const Text(
-            "General History",
-            style: TextStyle(
-                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+          Text(
+            topic.topicName,
+            style: const TextStyle(
+                fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          const Text(
-            "Dive into history! We will embark on a captivating exploration of the past, unraveling the rich tapestry of human history.",
+          Text(
+            topic.description,
             textAlign: TextAlign.justify,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 11,
               color: Colors.white,
             ),
