ca3c982c0af7eb0f560ad24841436b312c5c7902
viancie <viancie.ocena@gmail.com>
2023-12-30 17:54:31 +0800

    last na jud

--- lib/components/topicsearch_tile.dart
+++ lib/components/topicsearch_tile.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:quizzard/components/heart_button.dart';
+import 'package:quizzard/controller/datarepo.dart';
 import 'package:quizzard/model/topic.dart';
 import 'package:quizzard/pages/topicView_page.dart';
 
-class TopicSearch extends StatelessWidget {
+class TopicSearch extends StatefulWidget {
   final Topic topic;
   TopicSearch({super.key, required this.topic});
 
   @override
+  State<TopicSearch> createState() => _TopicSearchState();
+}
+
+class _TopicSearchState extends State<TopicSearch> {
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.push(context,
-          MaterialPageRoute(builder: (context) => TopicViewPage(topic: topic))),
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => TopicViewPage(topic: widget.topic))),
       child: Container(
         padding: const EdgeInsets.all(20),
         height: 70,
@@ -30,12 +38,24 @@ class TopicSearch extends StatelessWidget {
         child:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Text(
-            topic.getTopicName,
+            widget.topic.getTopicName,
             style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
           ),
           HeartButton(
-            isLiked: topic.getIsLiked,
-            onTap: () {},
+            isLiked: widget.topic.getIsLiked,
+            onTap: () {
+              setState(() {
+                if (widget.topic.liked) {
+                  widget.topic.setIsLiked("FALSE");
+                  DataRepository.favoriteList.remove(widget.topic);
+                } else {
+                  widget.topic.setIsLiked("TRUE");
+                  DataRepository.favoriteList.add(widget.topic);
+                }
+                widget.topic.liked = !widget.topic.liked;
+                DataRepository.updateTopicList();
+              });
+            },
             colorClicked: Colors.black,
             colorNotClicked: Colors.black,
           ),
